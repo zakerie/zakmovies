995c9a911f5c91c1bfe527dc537bfbe6686b0d8e
zakerie <tambisfred25@gmail.com>
2025-04-20 21:50:23 +0800

    Update home.js

--- my-website/my-website/js/home.js
+++ my-website/my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'aa34789c31e9543e363589ee233202ac';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
